f1c907e511537f23dbac292e50ad25f343244181
grandmasterdev <one.ahmad.arifin@gmail.com>
2021-09-07 21:59:42 +0800

    fixed apigateway custom domain stack test

--- apigateway-custom-domain/test/apigateway-custom-domain.test.ts
+++ apigateway-custom-domain/test/apigateway-custom-domain.test.ts
@@ -1,13 +1,13 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResourceLike } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as ApigatewayCustomDomain from '../lib/apigateway-custom-domain-stack';
 
-test('Empty Stack', () => {
+test('stack should have resources', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new ApigatewayCustomDomain.ApigatewayCustomDomainStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expectCDK(stack).to(haveResourceLike("AWS::Route53::HostedZone"))
+    expectCDK(stack).to(haveResourceLike("AWS::ApiGateway::DomainName"))
+    expectCDK(stack).to(haveResourceLike("AWS::CertificateManager::Certificate"))
 });
